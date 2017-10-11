cfdb09efeceba031d7d9e56b4aa93921232d71a6
config <ingcabo@gmail.com>
2017-10-11 18:50:08 -0300

    PRE-Inicio

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -5,14 +5,16 @@ require APPPATH.'/libraries/REST_Controller.php';
 
 class Products extends REST_Controller{
 
-	function __construct(){
+	function __construct()
+	{
 		parent:: __construct();
 		$this->load->helper('my_api_helper');
 		$this->load->model('Model_products');
 		$this->load->config('rest');
 	}
-	
-	public function index_get(){
+
+	public function index_get()
+	{
 
 		$criterio['activo'] = 1;
 		$data = $this->Model_products->get_many_by($criterio);
